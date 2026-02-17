42ff906f4a923e58c068d9f4a9f7b8daee8dfef7
oxiqod <oxiqod@mail.ru>
2026-02-17 11:18:36 +0300

    fix: backend set pool config settings

--- apps/backend/src/infrastructure/db/db.ts
+++ apps/backend/src/infrastructure/db/db.ts
@@ -4,11 +4,10 @@ import { Pool } from 'pg'
 export const db = new Kysely<any>({
   dialect: new PostgresDialect({
     pool: new Pool({
-      host: 'localhost',
-      port: 5432,
-      user: 'bodytracker',
-      password: 'bodytracker',
-      database: 'bodytracker',
+      connectionString: process.env.DATABASE_URL,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     }),
   }),
 })
